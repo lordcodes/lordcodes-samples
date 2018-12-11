15274803c9578a13acfada864a2cba6ec945707a
Andrew Lord <andrew@lordcodes.com>
2018-12-11 08:16:11 +0000

    Example file templates for Xcode file templates article

---
+++ ios/create-xcode-file-templates-and-share-them-with-your-team/Protocol Fake.xctemplate/___FILEBASENAME___Fake.swift
@@ -0,0 +1,9 @@
+//___FILEHEADER___
+
+@testable import MyApp
+
+class ___FILEBASENAMEASIDENTIFIER___ {
+}
+
+extension ___FILEBASENAMEASIDENTIFIER___: ___VARIABLE_productName___ {
+}
---
+++ ios/create-xcode-file-templates-and-share-them-with-your-team/UI Test.xctemplate/XCTestCaseSwift/___FILEBASENAME___UITests.swift
@@ -0,0 +1,18 @@
+//___FILEHEADER___
+
+import XCTest
+
+class ___FILEBASENAMEASIDENTIFIER___: ___VARIABLE_testSubclass___ {
+    private let app = XCUIApplication()
+
+    override func setUp() {
+        super.setUp()
+
+        continueAfterFailure = false
+    }
+}
+
+extension ___FILEBASENAMEASIDENTIFIER___ {
+    func test_() {
+    }
+}
