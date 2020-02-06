462e9f770d1d2adb9ce3bd72232d4fdd566e56b7
Andrew Lord <andrew@lordcodes.com>
2020-02-06 13:27:59 +0000

    Identifier type safety article sample code

---
+++ android/enforcing-type-safety-of-identifiers-in-kotlin/scratch.kts
@@ -0,0 +1,33 @@
+import java.util.UUID
+
+// General types
+
+data class Team(val id: UUID, val size: Int)
+data class TeamMember(val id: String, val name: String)
+
+// Wrapper type
+
+data class WrapperIdentifier(val rawValue: UUID)
+
+data class GenericIdentifier<RawT>(val rawValue: RawT)
+
+
+// Ensuring the correct entity
+
+data class Identifier<EntityT, RawT>(
+    val rawValue: RawT
+)
+
+typealias RoomId = Identifier<Room, UUID>
+data class Room(val id: RoomId)
+
+typealias MeetingId = Identifier<Meeting, UUID>
+data class Meeting(val id: MeetingId)
+
+val room = Room(RoomId(UUID.randomUUID()))
+val meeting = Meeting(MeetingId(UUID.randomUUID()))
+
+fun bookMeeting(id: MeetingId) {}
+
+//bookMeeting(room.id)
+bookMeeting(meeting.id)
